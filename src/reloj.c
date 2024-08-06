b1be84f11a60529384bb7b31a1f0dc843cf732d5
adelarja <adelarja@gmail.com>
2024-08-06 12:50:05 -0300

    added some unti tests

--- src/reloj.c
+++ src/reloj.c
@@ -1,47 +1,76 @@
 #include "reloj.h"
+#include <time.h>
 
 struct clock_s {
     uint8_t time[TIME_SIZE];
     uint16_t ticks_count;
     uint16_t ticks_per_second;
+    time_t elapsed_seconds;
     bool valid;
 };
 
-static struct clock_s clock;
+static struct clock_s clk;
 
-bool getCurrentTime(clock_t clock_ptr, uint8_t *time, int number_of_elements) {
+static void epoch_bcd_to_time_array(clock_ptr);
+static void bcd_to_time(clock_ptr);
+
+static void set_bcd_time_in_clock(clock_ptr self) {
+    // struct tm *local_time = localtime(&self->elapsed_seconds);
+
+    // self->time[0] = local_time->tm_hour / 10;
+    // self->time[1] = local_time->tm_hour % 10;
+
+    // self->time[2] = local_time->tm_min / 10;
+    // self->time[3] = local_time->tm_min % 10;
+
+    // self->time[4] = local_time->tm_sec / 10;
+    // self->time[5] = local_time->tm_sec % 10;
+    self->time[0] = (self->elapsed_seconds / 3600) / 10;
+    self->time[1] = (self->elapsed_seconds / 3600) % 10;
+
+    self->time[2] = ((self->elapsed_seconds % 3600) / 60) / 10;
+    self->time[3] = ((self->elapsed_seconds % 3600) / 60) % 10;
+
+    self->time[4] = ((self->elapsed_seconds % 3600) % 60) / 10;
+    self->time[5] = ((self->elapsed_seconds % 3600) % 60) % 10;
+}
+
+static void bcd_to_time(clock_ptr self) {
+    uint8_t hour = (self->time[0] * 10) + self->time[1];
+    uint8_t minute = (self->time[2] * 10) + self->time[3];
+    uint8_t second = (self->time[4] * 10) + self->time[5];
+
+    self->elapsed_seconds = (hour * SECONDS_IN_AN_HOUR) + (minute * SECONDS_IN_A_MINUTE) + second;
+}
+
+bool getCurrentTimeBCD(clock_ptr self, uint8_t *time, int number_of_elements) {
     memset(time, 0, number_of_elements);
-    memcpy(time, clock_ptr->time, sizeof(clock_ptr->time));
+    set_bcd_time_in_clock(self);
+    memcpy(time, self->time, sizeof(self->time));
     return false;
 }
 
-clock_t clockInit(uint8_t t) {
-    memset(clock.time, 0, TIME_SIZE);
-    clock.ticks_count = 0;
-    clock.ticks_per_second = t;
-    clock.valid = true;
+clock_ptr clockInit(uint8_t t) {
+    memset(clk.time, 0, TIME_SIZE);
 
-    return &clock;
+    clk.ticks_count = 0;
+    clk.ticks_per_second = t;
+    clk.elapsed_seconds = time(0);
+    clk.valid = true;
+
+    return &clk;
 }
 
-void setTime(clock_t clock_ptr, uint8_t *time, int size) {
-    memcpy(time, clock_ptr->time, sizeof(clock_ptr->time));
+void setTime(clock_ptr self, uint8_t *time, int size) {
+    memcpy(time, self->time, sizeof(self->time));
+    bcd_to_time(self);
 }
 
-// void incrementTick(uint8_t *time, uint8_t const size) {
-//     static uint8_t time_index = 0;
+void incrementTick(clock_ptr self) {
+    self->ticks_count += 1;
 
-//     if (time_[size - 1 - time_index] == 9) {
-//         time_[size - 1 - time_index] = 0;
-//         if(time_[size - 1 - time_index + 1] == 5) {
-//             time_[size - 1 - time_index + 1] = 0;
-//             time_[size - 1 - time_index + 2] += 1;
-//             if(time_[size - 1 - time_index + 2] == 9) {
-//                 time_[size - 1 - time_index + 2] = 0;
-//                 time_[size - 1 - time_index + 3] += 1;
-//             }
-//         }
-//     } 
-
-//     time_[size - 1 - time_index] += 1;
-// }
+    if(self->ticks_count == self->ticks_per_second) {
+        self->ticks_count = 0;
+        self->elapsed_seconds += 1;
+    }
+}
--- src/reloj.h
+++ src/reloj.h
@@ -3,10 +3,12 @@
 #include <string.h>
 
 #define TIME_SIZE 6
+#define SECONDS_IN_AN_HOUR 3600
+#define SECONDS_IN_A_MINUTE 60
 
-typedef struct clock_s *clock_t;
-
-bool getCurrentTime(clock_t, uint8_t *, int);
-clock_t clockInit(uint8_t);
-void incrementTick(uint8_t*, uint8_t);
-void setTime(clock_t, uint8_t*, int);
+typedef struct clock_s *clock_ptr;
+// typedef void (*clock_event_t)(clock_t);
+bool getCurrentTimeBCD(clock_ptr, uint8_t *, int);
+clock_ptr clockInit(uint8_t);
+void incrementTick(clock_ptr);
+void setTime(clock_ptr, uint8_t*, int);
